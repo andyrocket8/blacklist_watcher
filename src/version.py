e8b825b7c84a3e19f2548d848d880c41c940929e
Dmitriev Andrey <dmitriev.andrey@customscard.ru>
2024-01-10 11:09:42 +0300

    Add version info for deployment. Change queue storage type for performance improvement

---
+++ src/version.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class Version:
+    major: int
+    minor: int
+    patch: int
+
+
+# Change application version here
+VERSION = {'major': 1, 'minor': 0, 'patch': 0}
+
+VERSION_INFO = Version(**VERSION)
+
+
+def get_version():
+    return '{}.{}.{}'.format(VERSION_INFO.major, VERSION_INFO.minor, VERSION_INFO.patch)
